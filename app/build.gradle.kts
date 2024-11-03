930119a7ec27648a17863dc14080eaaebef28ab9
Banu Eka Prayoga <banu.ekaprayoga20@gmail.com>
2024-11-04 00:00:20 +0700

    UI Update camera dan profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dicoding.capstoneproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dicoding.capstoneproject"
