ed8ba5acd489e045baefca8ec2446575379c8d6f
Banu Eka Prayoga <banu.ekaprayoga20@gmail.com>
2024-11-07 19:28:42 +0700

    Update UI Profile

--- app/src/main/java/com/example/capstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/capstone/ui/home/HomeFragment.kt
@@ -7,28 +7,55 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.capstone.R
 import com.example.capstone.databinding.FragmentHomeBinding
+import com.example.capstone.ui.adapter.ListPlantAdapter
+import com.example.capstone.ui.adapter.Plant
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var listPlantAdapter: ListPlantAdapter
+    private val list = ArrayList<Plant>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        list.addAll(getListPlant())
+        setupRecyclerView()
+    }
+    private fun getListPlant(): ArrayList<Plant> {
+        val dataName = resources.getStringArray(R.array.data_nama)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+
+        val listPlant = ArrayList<Plant>()
+        for (i in dataName.indices) {
+            val plant = Plant(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listPlant.add(plant)
+        }
+        return listPlant
+    }
+    private fun setupRecyclerView() {
+        listPlantAdapter = ListPlantAdapter(list)
+        binding.rvPlants.layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvPlants.adapter = listPlantAdapter
+        binding.rvPlants.setHasFixedSize(true)
+
+//        val itemDecoration = DividerItemDecoration(requireActivity(), DividerItemDecoration.VERTICAL)
+//        binding.rvPlants.addItemDecoration(itemDecoration)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/capstone/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/capstone/ui/profile/ProfileFragment.kt
@@ -12,9 +12,6 @@ import com.example.capstone.databinding.FragmentProfileBinding
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +19,13 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
